5619d35d2d645440441e2e2200889999efa2aaab
Radu Edward-Andrei <raduedwardandrei97@yahoo.com>
2020-06-29 00:44:13 +0300

    Updates

---
+++ precache-manifest.d7fff6ee936f755914fafb4094e6ae1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e7a1bd97561dfbbc8e5de237564c20",
+    "url": "/RaduEdwardAndrei/MovieFlex2.git/index.html"
+  },
+  {
+    "revision": "b1fb98421f049366b865",
+    "url": "/RaduEdwardAndrei/MovieFlex2.git/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "40c50d5416771e407ad3",
+    "url": "/RaduEdwardAndrei/MovieFlex2.git/static/css/main.c20dc4c8.chunk.css"
+  },
+  {
+    "revision": "b1fb98421f049366b865",
+    "url": "/RaduEdwardAndrei/MovieFlex2.git/static/js/2.c6636483.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/RaduEdwardAndrei/MovieFlex2.git/static/js/2.c6636483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c50d5416771e407ad3",
+    "url": "/RaduEdwardAndrei/MovieFlex2.git/static/js/main.9ce3b4b2.chunk.js"
+  },
+  {
+    "revision": "7b5168cade3210651ee2",
+    "url": "/RaduEdwardAndrei/MovieFlex2.git/static/js/runtime-main.e7a328d2.js"
+  },
+  {
+    "revision": "efe4fbe85e64f615e00ff789de334780",
+    "url": "/RaduEdwardAndrei/MovieFlex2.git/static/media/backgroundpic.efe4fbe8.png"
+  }
+]);
